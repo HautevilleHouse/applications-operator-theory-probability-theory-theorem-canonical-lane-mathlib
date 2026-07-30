3af334828b341e98f62a90c1c49d0620a6d524f2
Hauteville House <pgjkmqpw52@privaterelay.appleid.com>
2026-07-29 20:35:44 -0400

    Canon

---
+++ applications_operator_theory_probability_theory_theorem_canonical_lane_mathlib/FinalTheorem.lean
@@ -0,0 +1,9 @@
+import applications_operator_theory_probability_theory_theorem_canonical_lane_mathlib.GateLemmas
+namespace HautevilleHouse
+namespace applications_operator_theory_probability_theory_theorem_canonical_lane_mathlib
+structure ConstrainedTheoremClosure where
+  admissible :: intro :: constraint :: consequence :: proof ::
+theorem canonical : ConstrainedTheoremClosure :=
+  ConstrainedTheoremClosure.intro
+end applications_operator_theory_probability_theory_theorem_canonical_lane_mathlib
+end HautevilleHouse
